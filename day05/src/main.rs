7069312c77e167a2e00d7e5d1deecb883b36d7fa
Kevin Kearney <kjkearney98@gmail.com>
2025-12-05 19:11:14 -0600

    don't build a hashset

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,41 +1,53 @@
 use std::collections::HashSet;
 use std::ops::RangeInclusive;
 
-const INPUT: &str = include_str!("../res/input");
 
+const INPUT0: &str = include_str!("../res/input0");
+const INPUT1: &str = include_str!("../res/input1");
 
+type RangeSet = HashSet<RangeInclusive<u64>>;
 
-fn parse_file(input :&str) -> (HashSet<u64>, Vec<u64>)
+fn parse_file() -> (RangeSet, Vec<u64>)
 {
-    let (ranges,values) = input.split_at(input.find("\r\n\r\n").unwrap());
 
-    let ingredient_ranges = ranges
+    let ingredient_ranges = INPUT0.trim()
         .lines()
         .into_iter()
         .map( |line| {
             let range = line.split_at(line.find('-').unwrap());
-            RangeInclusive::new(range.0.parse::<u64>().unwrap(),range.1[1..].parse::<u64>().unwrap())
-        })
-        .flatten();
+            RangeInclusive::new(
+                range.0.parse::<u64>().unwrap(),
+                range.1[1..].parse::<u64>().unwrap()
+            )
+        }).collect::<RangeSet>();
 
-    let test_ingredients = values.trim().lines().map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+    let test_ingredients = INPUT1
+        .trim()
+        .lines()
+        .map(|s|
+            s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
 
-    (HashSet::from_iter(ingredient_ranges),test_ingredients)
+    (ingredient_ranges,test_ingredients)
 }
 
-fn solve_2(input: &str) -> u32 {
+fn solve_2() -> u32 {
     0
 }
 
-fn solve_1(input: &str) -> u32 {
-    let (good_ingredients, pantry) = parse_file(input);
+fn solve_1() -> u32 {
 
-    pantry.iter().filter(|i| good_ingredients.contains(i)).count() as u32
-
+    let (good_ingredients, pantry) = parse_file();
+    pantry
+        .iter()
+        .filter(|i|
+            good_ingredients
+                .iter()
+                .any(|r| r.contains(i)))
+        .count() as u32
 }
 
 fn main()
 {
-    println!("{}", solve_1(INPUT));
-    println!("{}", solve_2(INPUT));
+    println!("{}", solve_1());
+    println!("{}", solve_2());
 }
