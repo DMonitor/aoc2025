52ad8c1095929ce4db93ee101352b0ca9e91d76a
Kevin Kearney <kjkearney98@gmail.com>
2025-12-05 19:03:24 -0600

    Initial go at day05

--- day05/src/main.rs
+++ day05/src/main.rs
@@ -1,18 +1,41 @@
+use std::collections::HashSet;
+use std::ops::RangeInclusive;
 
-const INPUT: &str = include_str!("../res/test");
+const INPUT: &str = include_str!("../res/input");
+
+
+
+fn parse_file(input :&str) -> (HashSet<u64>, Vec<u64>)
+{
+    let (ranges,values) = input.split_at(input.find("\r\n\r\n").unwrap());
+
+    let ingredient_ranges = ranges
+        .lines()
+        .into_iter()
+        .map( |line| {
+            let range = line.split_at(line.find('-').unwrap());
+            RangeInclusive::new(range.0.parse::<u64>().unwrap(),range.1[1..].parse::<u64>().unwrap())
+        })
+        .flatten();
+
+    let test_ingredients = values.trim().lines().map(|s| s.parse::<u64>().unwrap()).collect::<Vec<u64>>();
+
+    (HashSet::from_iter(ingredient_ranges),test_ingredients)
+}
 
 fn solve_2(input: &str) -> u32 {
     0
 }
 
 fn solve_1(input: &str) -> u32 {
-    0
+    let (good_ingredients, pantry) = parse_file(input);
+
+    pantry.iter().filter(|i| good_ingredients.contains(i)).count() as u32
+
 }
 
 fn main()
 {
-
-
     println!("{}", solve_1(INPUT));
     println!("{}", solve_2(INPUT));
 }
