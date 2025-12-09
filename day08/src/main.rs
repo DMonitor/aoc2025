15acab5507462fb5c0ce389d4a3e4073f6ba5af5
Kevin Kearney <kjkearney98@gmail.com>
2025-12-09 15:20:04 -0600

    Create a shared library

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,29 +1,29 @@
 use std::collections::{HashSet};
-use lib::points::Point;
+use lib::points::Point3D;
 
 const INPUT: &str = include_str!("../res/input");
 
-fn parse_input(input:&str) -> HashSet<Point>
+fn parse_input(input:&str) -> HashSet<Point3D>
 {
     input
         .lines()
         .map(|line|
             line.split_terminator(',').map(|x| x.parse::<i64>().unwrap()))
         .map(|mut points|
-            Point {x: points.next().unwrap(), y: points.next().unwrap(), z: points.next().unwrap()})
+            Point3D {x: points.next().unwrap(), y: points.next().unwrap(), z: points.next().unwrap()})
         .collect()
 }
 
 fn solve_2(input: &str) -> u64 {
     let point_map = parse_input(input);
 
-    let mut relations:Vec<(Point, Point, i64)> =
-        point_map.iter().map( |point|
+    let mut relations:Vec<(Point3D, Point3D, i64)> =
+        point_map.iter().map( |Point3D|
             point_map
                 .iter()
-                .map(|p| (p,p.distance_cmp(*point)))
+                .map(|p| (p,p.distance_cmp(*Point3D)))
                 .filter(|p| p.1 != 0)
-                .map(|p| (point.clone(),*p.0,p.1)).collect::<Vec<(Point, Point, i64)>>())
+                .map(|p| (Point3D.clone(),*p.0,p.1)).collect::<Vec<(Point3D, Point3D, i64)>>())
             .flatten().collect();
 
     // sorted with closest at the bottom
@@ -32,9 +32,9 @@ fn solve_2(input: &str) -> u64 {
     let goal = point_map.len();
 
     // each circuit having 2 cells is a fair assumption
-    let mut circuits = Vec::<HashSet<Point>>::with_capacity(goal /2);
+    let mut circuits = Vec::<HashSet<Point3D>>::with_capacity(goal /2);
 
-    let (mut x, mut y):(Point,Point) = (Point {x:0,y:0,z:0},Point {x:0,y:0,z:0});
+    let (mut x, mut y):(Point3D,Point3D) = (Point3D {x:0,y:0,z:0},Point3D {x:0,y:0,z:0});
 
     while !circuits.iter().any(|c| c.len() >= goal)
     {
@@ -42,7 +42,7 @@ fn solve_2(input: &str) -> u64 {
         x = close.0;
         y = close.1;
         relations.pop();
-        let mut neighbor_circuits = circuits.iter_mut().filter( |c| c.contains(&close.0) || c.contains(&close.1)).collect::<Vec<&mut HashSet<Point>>>();
+        let mut neighbor_circuits = circuits.iter_mut().filter( |c| c.contains(&close.0) || c.contains(&close.1)).collect::<Vec<&mut HashSet<Point3D>>>();
 
         //println!("{} {}", close.0, close.1);
         match neighbor_circuits.len()
@@ -50,7 +50,7 @@ fn solve_2(input: &str) -> u64 {
             0 => {
                 println!("new");
                 // make a new circuit
-                let mut new_circuit:HashSet<Point> = HashSet::new();
+                let mut new_circuit:HashSet<Point3D> = HashSet::new();
                 new_circuit.insert(close.0);
                 new_circuit.insert(close.1);
                 circuits.push(new_circuit);
@@ -83,14 +83,14 @@ fn solve_1(input: &str) -> u64 {
 
     let point_map = parse_input(input);
 
-    let mut relations:Vec<(Point, Point, i64)> = Vec::with_capacity(point_map.len());
+    let mut relations:Vec<(Point3D, Point3D, i64)> = Vec::with_capacity(point_map.len());
 
-    for point in point_map.iter()
+    for Point3D in point_map.iter()
     {
-        let me = point.clone();
+        let me = Point3D.clone();
         let closest = point_map
                 .iter()
-                .map(|p| (p,p.distance_cmp(*point)))
+                .map(|p| (p,p.distance_cmp(*Point3D)))
                 .filter(|p| p.1 != 0)
                 .map(|p| (me,*p.0,p.1));
         relations.extend(closest);
@@ -102,14 +102,14 @@ fn solve_1(input: &str) -> u64 {
     const LINKS:usize = 10;
 
     // each circuit having 2 cells is a fair assumption
-    let mut circuits = Vec::<HashSet<Point>>::with_capacity(LINKS/2);
+    let mut circuits = Vec::<HashSet<Point3D>>::with_capacity(LINKS/2);
     let mut made_links = 0;
 
     while made_links < LINKS
     {
         let close = relations.pop().unwrap();
         relations.pop();
-        let mut neighbor_circuits = circuits.iter_mut().filter( |c| c.contains(&close.0) || c.contains(&close.1)).collect::<Vec<&mut HashSet<Point>>>();
+        let mut neighbor_circuits = circuits.iter_mut().filter( |c| c.contains(&close.0) || c.contains(&close.1)).collect::<Vec<&mut HashSet<Point3D>>>();
 
         println!("{} {}", close.0, close.1);
         match neighbor_circuits.len()
@@ -117,7 +117,7 @@ fn solve_1(input: &str) -> u64 {
             0 => {
                 println!("new");
                 // make a new circuit
-                let mut new_circuit:HashSet<Point> = HashSet::new();
+                let mut new_circuit:HashSet<Point3D> = HashSet::new();
                 new_circuit.insert(close.0);
                 new_circuit.insert(close.1);
                 circuits.push(new_circuit);
--- lib/src/points.rs
+++ lib/src/points.rs
@@ -1,24 +1,46 @@
 use std::fmt::{Display, Formatter};
 #[derive(PartialEq,Eq,Hash,Debug,Copy,Clone)]
-pub struct Point
+pub struct Point3D
 {
     pub x:i64,
     pub y:i64,
     pub z:i64,
 }
 
-impl Point
+impl Point3D
 {
     // distance without sqrt, good enough for comparisons
-    pub fn distance_cmp(self, other:Point) -> i64
+    pub fn distance_cmp(self, other:Point3D) -> i64
     {
         (self.x - other.x).pow(2) + (self.y - other.y).pow(2) + (self.z - other.z).pow(2)
     }
 }
 
-impl Display for Point
+impl Display for Point3D
 {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         f.write_fmt(format_args!("({}, {}, {})", self.x, self.y, self.z))
+    }
+}
+
+
+
+#[derive(PartialEq,Eq,Hash,Debug,Copy,Clone)]
+pub struct Point2D
+{
+    pub x:i64,
+    pub y:i64,
+}
+
+impl Point2D {
+    pub fn area_rectangle(&self, other: &Point2D) -> i64 {
+        ((self.x-other.x + 1) * (self.y-other.y + 1)).abs()
+    }
+}
+
+impl Display for Point2D
+{
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        f.write_fmt(format_args!("({}, {})", self.x, self.y))
     }
 }
