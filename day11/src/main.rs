1be39359474d5b9c97e2b9fb071c47612877160a
Kevin Kearney <kjkearney98@gmail.com>
2025-12-11 01:16:32 -0600

    Add day 11

---
+++ day11/src/main.rs
@@ -0,0 +1,18 @@
+
+const INPUT: &str = include_str!("../res/test");
+
+fn solve_2(input: &str) -> u32 {
+    0
+}
+
+fn solve_1(input: &str) -> u32 {
+    0
+}
+
+fn main()
+{
+
+
+    println!("{}", solve_1(INPUT));
+    println!("{}", solve_2(INPUT));
+}
